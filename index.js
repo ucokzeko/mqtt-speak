725317c2b40b74276d4da2b193766e83ae804454
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-04-13 16:59:39 +1000

    ENHANCE: Moved audio processing out of index file

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var mqtt = require('mqtt');
 var crypto = require('crypto');
 var fs = require('fs');
-var includes = require('array-includes');
-var player = require('play-sound')(opts = {});
 var Processor = require('./module/audio-processor.js');
 var config = require('config.json')('./config.json');
 
 var client = mqtt.connect('mqtt://localhost');
-var processor = new Processor();
 
 client.on('connect', function () {
   	client.subscribe(config.channel.sub);
@@ -19,35 +16,13 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
 	var md5String = getMD5(message.toString());
-  	console.log(message.toString() + ": " + md5String);
-  	if (isAudioExist(md5String)) {
-  		// TODO: Action when audio file is found
-  		console.log('Audio found.');
-  		playAudio(md5String);
-  	} else {
-  		// Request TTS service to generate audio file and save it
-  		console.log('Generating audio file.');
-  		processor.createAudioFile(message.toString(), config.audio.path, md5String + ".mp3", function(err, data) {
-  			if (err) {
-  				console.error('Creating audio file failed.\n', err);
-  			} else {
-  				playAudio(md5String);
-  			}
-  		});
-  	}
-	console.log('--- Done processing request ---');
+	var filename = md5String + ".mp3";
+  	console.log('--- Start processing request for: ' + message.toString() + " (" + md5String + ") ---");
+  	new Processor(message.toString(), config.audio.path, filename, function (error, path) {
+  		if (error) { console.error ("Failed getting audio file.\n", e); }
+  		else { client.publish(config.channel.pub, path); }
+  	});
 });
-
-function playAudio(md5String) {
-	player.play(config.audio.path + "/" + md5String + '.mp3', function(err){
-		console.log('Audio played.');
-	});
-}
-
-function isAudioExist(md5String) {
-	var files = fs.readdirSync(config.audio.path);
-	return includes(files, md5String + '.mp3');
-}
 
 function getMD5(text) {
 	return crypto.createHash('md5').update(text).digest("hex");
--- module/audio-processor.js
+++ module/audio-processor.js
@@ -1,19 +1,33 @@
 var fs = require('fs');
 var request = require('request');
+var includes = require('array-includes');
 
-var Proc = function() {};
+var Proc = function(msgString, path, filename, callback) {
+	if (isFileExist(path, filename)) {
+  		// TODO: Action when audio file is found
+  		console.log('Audio found.');
+  		callback(null, path + filename)
+  	} else {
+  		// Request TTS service to generate audio file and save it
+		request(getTTSRequestUrl(msgString), function (error, response, body) {
+			if (error) { return callback(error, ''); }
+			else {
+				if (response.statusCode == 200) {
+			    	var audioUrl = JSON.parse(body).snd_url.replace('\\','');
+			        console.log('Audio url: ' + audioUrl); // Show download link.
+			        downloadAudioFile(audioUrl, path, filename, function(err, data) {
+		  				if (err) { return callback(err, ''); }
+		  				callback(null, data);
+		        	});
+			    } else { return callback(new Error('Response is unexpected: ' + response.statusCode), null); }
+			}
+		})
+  	}
+};
 
-Proc.prototype.createAudioFile = function(messageString, audioPath, fileName, callback) {
-	request(getTTSRequestUrl(messageString), function (error, response, body) {
-	    if (!error && response.statusCode == 200) {
-	    	var audioUrl = JSON.parse(body).snd_url.replace('\\','');
-	        console.log('Audio url: ' + audioUrl); // Show download link.
-	        downloadAudioFile(audioUrl, audioPath, fileName, function(err, data) {
-  				if (err) { return callback(err); }
-  				callback(null, data);
-	        });
-	    } else { return callback(error); }
-	});
+function isFileExist(path, filename) {
+	var files = fs.readdirSync(path);
+	return includes(files, filename);
 }
 
 function getTTSRequestUrl(reqText) {
@@ -21,12 +35,12 @@ function getTTSRequestUrl(reqText) {
 	return baseUrl + '?req_voice=lisa22k&req_text="' + reqText + '"&prot_vers=2&cl_login=EVAL_VAAS&cl_app=EVAL_3608771&cl_pwd=du40md9t&req_asw_type=SOUND'
 }
 
-function downloadAudioFile(audioUrl, audioPath, fileName, callback) {
-	request(audioUrl, {encoding: 'binary'}, function(error, response, body) {
-  		fs.writeFile(audioPath + fileName, body, 'binary', function(err) {
-  			if (err) { return callback(err); }
+function downloadAudioFile(url, path, filename, callback) {
+	request(url, {encoding: 'binary'}, function(error, response, body) {
+  		fs.writeFile(path + filename, body, 'binary', function(err) {
+  			if (err) { return callback(err, ''); }
 			console.log('Audio file saved.');
-			callback (null, true);
+			callback (null, path + filename);
   		});
 	});
 }
