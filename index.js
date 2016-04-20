1016423a8222e9bc152eb4de159626c3146a95bd
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-04-20 17:46:40 +1000

    NEW: Add .eslintrc.json from detox-api

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const config = require('config.json')('./config.json');
 const Processor = require('./module/audio-processor.js');
 const client = mqtt.connect('mqtt://localhost');
 
-client.on('connect', function () {
+client.on('connect', () => {
   client.subscribe(config.channel.sub);
-  
   try {
     fs.lstatSync(config.audio.path);
   } catch (e) {
     fs.mkdirSync(config.audio.path);
   }
 });
- 
-client.on('message', function (topic, message) {
-  console.log('-- Message received ---');
-  console.log('Message: ' + message.toString());
+
+client.on('message', (topic, message) => {
   new Processor(message.toString(), config.audio.path)
-  .then(function (path) {
+  .then((path) => {
     client.publish(config.channel.pub, path);
-    console.log('Put audio path to playing queue');
-  }, function(error) {
-    console.error('Failed getting audio file.\n', error);
+  }, (error) => {
+    throw error;
   });
 });
