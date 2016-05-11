8470935fb37a9dc70139e2e934726de66bd62b7b
Rhys <rimmington@gmail.com>
2016-05-11 16:16:44 +1000

    CONFIG: Nix service description

---
+++ service.nix
@@ -0,0 +1,13 @@
+{mkService, callPackage, nodejs, mqtt-speak ? callPackage ./default.nix {}, mosquitto}:
+
+mkService {
+  name = "mqtt-speak";
+  dependsOn = [ mosquitto ];
+  environment = {
+    ACAPELA_TTS_LOGIN = "YOUR_LOGIN";
+    ACAPELA_TTS_APP = "YOUR_APP";
+    ACAPELA_TTS_PWD = "YOUR_PWD";
+    SPEAK_AUDIO_PATH = "/tmp/mqtt-speak/";
+  };
+  script = "exec ${nodejs}/bin/node --use_strict ${mqtt-speak.build}/lib/node_modules/mqtt-speak/src/index.js";
+}
