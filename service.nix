0b7aaca8f49b2f1a326d53c634915f021196b91e
Rhys <rimmington@gmail.com>
2016-07-14 10:36:52 +1000

    CONFIG: Pass user.name to mkService (now mandatory)

--- service.nix
+++ service.nix
@@ -7,5 +7,6 @@ mkService {
     DETOX_CENTRAL_URL = "SOME_URL";
     SPEAK_AUDIO_PATH = "/tmp/mqtt-speak/";
   };
+  user.name = "mqtt-speak";
   script = "exec ${nodejs}/bin/node --use_strict ${mqtt-speak.build}/lib/node_modules/mqtt-speak/src/index.js";
 }
