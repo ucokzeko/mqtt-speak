8402bb9f512e2344b0c8b99cd69f3574bcc782f5
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-08-08 10:02:43 +1000

    ENHANCE: Setup shared audio files directory in mqtt-speak home

--- service.nix
+++ service.nix
@@ -1,11 +1,25 @@
 {mkService, callPackage, nodejs, mqtt-speak ? callPackage ./release.nix {}, mosquitto}:
 
-mkService {
+mkService rec {
   name = "mqtt-speak";
+
+  user.name = "mqtt-speak";
+  user.home = "/var/lib/${user.name}";
+
   dependsOn = [ mosquitto ];
   environment = {
-    SPEAK_AUDIO_PATH = "/tmp/mqtt-speak/";
+    SPEAK_AUDIO_PATH = "/var/lib/${user.name}/audio-files/";
   };
-  user.name = "mqtt-speak";
-  script = "exec ${nodejs}/bin/node --use_strict ${mqtt-speak.build}/lib/node_modules/mqtt-speak/src/index.js";
+    
+  script = "
+    if [ -d /var/lib/${user.name}/audio-files/ ]
+    then
+      echo '/var/lib/${user.name}/audio-files/ found.'
+    else
+      mkdir /var/lib/${user.name}/audio-files
+    fi
+
+    chmod -R o+rx ~/
+    exec ${nodejs}/bin/node --use_strict ${mqtt-speak.build}/lib/node_modules/mqtt-speak/src/index.js
+  ";
 }
