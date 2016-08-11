e0ccf2e9eebfd7390b1bd9174b2d78e7b27de9c3
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-08-11 13:13:32 +1000

    FIX: Add delay before checking downloaded file

--- test/integration/test_module/mqtt_tests.js
+++ test/integration/test_module/mqtt_tests.js
@@ -10,49 +10,59 @@ const client = mqtt.connect(consts.mqttHost);
 const mqttConfig = { qos: 1 };
 
 describe('MQTT', () => {
-  let message     = 'This is test audio';
   const topic     = consts.speakTopic.replace('#', 'test');
   const processor = rewire(`${__dirname}/../../../src/module/tts-processor.js`);
 
   describe('MQTT#publish message', () => {
     it('should not throw error when message contains only alphabetic characters', (done) => {
-      winston.info(`Publishing message to topic ${topic}`);
-      client.publish(topic, formatMqttMessage(message), mqttConfig);
+      const message  = 'This is test audio';
       const filename = processor.__get__('getMD5String')(message);
 
-      assert.doesNotThrow(() => {
-        isFileCreated(`${consts.audioPath}${filename}.mp3`);
-      }, (error) =>
-        error
-      );
-      done();
+      winston.info(`Publishing message to topic ${topic}`);
+      client.publish(topic, formatMqttMessage(message), mqttConfig);
+
+      setTimeout(() => {
+        assert.doesNotThrow(() => {
+          isFileCreated(`${consts.audioPath}${filename}.mp3`);
+        }, (error) =>
+          error
+        );
+        done();
+      }, 1000 * 1);
     });
 
     it('should not throw error when message contains combination alphabetic and numeric characters', (done) => {
-      message = 'Test 123';
-      winston.info(`Publishing message to topic ${topic}`);
-      client.publish(topic, formatMqttMessage(message), mqttConfig);
+      const message  = 'Test 123';
       const filename = processor.__get__('getMD5String')(message);
 
-      assert.doesNotThrow(() => {
-        isFileCreated(`${consts.audioPath}${filename}.mp3`);
-      }, (error) =>
-        error
-      );
-      done();
+      winston.info(`Publishing message to topic ${topic}`);
+      client.publish(topic, formatMqttMessage(message), mqttConfig);
+
+      setTimeout(() => {
+        assert.doesNotThrow(() => {
+          isFileCreated(`${consts.audioPath}${filename}.mp3`);
+        }, (error) =>
+          error
+        );
+        done();
+      }, 1000 * 1);
     });
 
     it('should not throw error when sending repeated message', (done) => {
-      winston.info(`Publishing message to topic ${topic}`);
-      client.publish(topic, formatMqttMessage(message), mqttConfig);
+      const message  = 'Test 123';
       const filename = processor.__get__('getMD5String')(message);
 
-      assert.doesNotThrow(() => {
-        isFileCreated(`${consts.audioPath}${filename}.mp3`);
-      }, (error) =>
-        error
-      );
-      done();
+      winston.info(`Publishing message to topic ${topic}`);
+      client.publish(topic, formatMqttMessage(message), mqttConfig);
+
+      setTimeout(() => {
+        assert.doesNotThrow(() => {
+          isFileCreated(`${consts.audioPath}${filename}.mp3`);
+        }, (error) =>
+          error
+        );
+        done();
+      }, 1000 * 1);
     });
   });
 });
