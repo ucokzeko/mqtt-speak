f74d0aab59087e486e335cb2ae737963b1adce43
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-09-21 14:47:59 +1000

    FIX: Http requests don't receive status code

--- src/module/detox-central-tts.js
+++ src/module/detox-central-tts.js
@@ -1,7 +1,6 @@
 const fs      = require('fs');
 const winston = require('winston');
 const request = require('request');
-const parse   = require('url-parse');
 
 const consts = require('../support/constants');
 
@@ -10,22 +9,21 @@ function fetch(message, path) {
     request.post(buildURL('tts'), { json: { message } }, (err, response, body) => {
       if (err) {
         reject(err);
+        return;
+      }
+      if (body.relative_url) {
+        const downloadUrl = buildURL(body.relative_url);
+        winston.info(`Download url: ${downloadUrl}`);
+        downloadAudioFile(downloadUrl, path).then((audioPath) => { fulfill(audioPath); }, reject);
       } else {
-        if (response.statusCode === 200 || response.statusCode === 201) {
-          const downloadUrl = buildURL(body.relative_url);
-          downloadAudioFile(downloadUrl, path).then((audioPath) => { fulfill(audioPath); }, reject);
-        } else {
-          reject(new Error(`Response is unexpected! ${response.statusCode} : ${body}`));
-        }
+        reject(new Error(`Response is unexpected! ${response.statusCode} : ${JSON.stringify(body, null, 2)}`));
       }
     });
   });
 }
 
 function buildURL(path) {
-  const url = parse(consts.detoxCentralAddress, true);
-  url.set('pathname', `/${path}`);
-  return url.toString();
+  return `${consts.detoxCentralAddress}/${path}`;
 }
 
 function downloadAudioFile(url, path) {
@@ -33,18 +31,19 @@ function downloadAudioFile(url, path) {
     request(url, { encoding: 'binary' }, (error, response, body) => {
       if (error) {
         reject(error);
+        return;
+      }
+      if (body) {
+        fs.writeFile(path, body, 'binary', (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          winston.info(`Audio file saved to ${path}`);
+          fulfill(path);
+        });
       } else {
-        if (response.statusCode === 200) {
-          fs.writeFile(path, body, 'binary', (err) => {
-            if (err) {
-              reject(err);
-            }
-            winston.info(`Audio file saved to ${path}`);
-            fulfill(path);
-          });
-        } else {
-          reject(error);
-        }
+        reject(new Error(`Audio file is not available. ${error}`));
       }
     });
   });
