2cc57c92a50353eb9dbb20e49c6b38557a38904b
Ewoudt Kellerman <ewoudt.kellerman@gmail.com>
2016-10-27 10:11:41 +1100

    FIX: Add authentication when interacting with central

--- src/module/detox-central-tts.js
+++ src/module/detox-central-tts.js
@@ -2,20 +2,29 @@ const fs      = require('fs');
 const winston = require('winston');
 const request = require('request');
 const parse   = require('url-parse');
+const util    = require('util');
 
 const consts = require('../support/constants');
+const auth   = require('detox-node-service-auth-module');
 
 function fetch(message, path) {
   return new Promise((fulfill, reject) =>  {
-    request.post(buildURL('tts'), { json: { message } }, (err, response, body) => {
-      if (err) {
-        reject(err);
-      } else if (response.statusCode === 200 || response.statusCode === 201) {
-        const downloadUrl = buildURL(body.relative_url);
-        downloadAudioFile(downloadUrl, path).then((audioPath) => { fulfill(audioPath); }, reject);
-      } else {
-        reject(new Error(`Response is unexpected! ${response.statusCode} : ${body}`));
-      }
+    auth.getCentralOptions
+    .then((centralOptions) => {
+      const options  = Object.assign({}, centralOptions);
+      options.json   = { message };
+      options.url    = buildURL('tts');
+      options.method = 'POST';
+      request(options, (err, response, body) => {
+        if (err) {
+          reject(err);
+        } else if (response.statusCode === 200 || response.statusCode === 201) {
+          const downloadUrl = buildURL(body.relative_url);
+          downloadAudioFile(centralOptions, downloadUrl, path).then((audioPath) => { fulfill(audioPath); }, reject);
+        } else {
+          reject(new Error(`Response is unexpected! ${response.statusCode} : ${util.inspect(body)}`));
+        }
+      });
     });
   });
 }
@@ -26,9 +35,11 @@ function buildURL(path) {
   return url.toString();
 }
 
-function downloadAudioFile(url, path) {
+function downloadAudioFile(options, url, path) {
   return new Promise((fulfill, reject) => {
-    request(url, { encoding: 'binary' }, (error, response, body) => {
+    const requestOptions    = options;
+    requestOptions.encoding = 'binary';
+    request(url, requestOptions, (error, response, body) => {
       if (error) {
         reject(error);
       } else if (response.statusCode === 200) {
